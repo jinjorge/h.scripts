1810b83740752c4480ec8cdc494ea3fd7e4014cc
Jeremiah Njoroge <jnjoroge@gmail.com>
2016-01-07 20:53:13 -0800

    Reference to icanhazip web page

--- ican.sh
+++ ican.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 # Simple script to run as a cron job to output the networks external IP address
+# Read more about icanhazip here - https://major.io/icanhazip-com-faq/
 DATE=`date +"%T"`
 
 echo -n "${DATE} " ; curl -4 https://icanhazip.com/
