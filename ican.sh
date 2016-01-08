146ef1cbe5d3a40b2329565f9c8131607b3577a9
Jeremiah Njoroge <jnjoroge@gmail.com>
2016-01-07 20:56:02 -0800

    Ican script output sample

--- ican.sh
+++ ican.sh
@@ -6,6 +6,10 @@ DATE=`date +"%T"`
 
 echo -n "${DATE} " ; curl -4 https://icanhazip.com/
 
+## Output of the script looks like
+## ➜  ~  ~/h.scripts/ican.sh
+## 17:17:35 104.60.96.66
+
 ### To run it as a cron job on the Mac, follow directions here on how to install a crontab
 ## http://benr75.com/pages/using_crontab_mac_os_x_unix_linux
 ## To disable the crontab, type 'crontab -e' and add a # to the start of the line
