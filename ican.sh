1047f509bf5941e4a2879121cd680c2f9e61d2c9
Jeremiah Njoroge <jnjoroge@gmail.com>
2016-01-07 20:36:23 -0800

    Icanzip cron script

---
+++ ican.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Simple script to run as a cron job to output the networks external IP address
+DATE=`date +"%T"`
+
+echo -n "${DATE} " ; curl -4 https://icanhazip.com/
+
+### To run it as a cron job on the Mac, follow directions here on how to install a crontab
+## http://benr75.com/pages/using_crontab_mac_os_x_unix_linux
+## To disable the crontab, type 'crontab -e' and add a # to the start of the line
